f5ea3544930eee1b9c360ffe32e2531b562ea0ad
rveliev <rveliev@ourgapps.com>
2023-02-11 19:25:01 +0300

    update server&client. Add test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,16 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"yalerting/cmd/handlers"
+	storageClient "yalerting/cmd/storage"
 )
 
-func HandleMetric(w http.ResponseWriter, r *http.Request) {
-	//w.Write([]byte("store or smth"))
-	//fmt.Println(r)
-}
-
 func main() {
+	storage := storageClient.NewMemStorage()
+
 	// маршрутизация запросов обработчику
-	http.HandleFunc("/", HandleMetric)
+	http.HandleFunc("/", handlers.HandleMetric(storage))
 	// запуск сервера с адресом localhost, порт 8080
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module yalerting
 
 go 1.19
+
+
+require (
+	github.com/stretchr/testify v1.7.1
+)
