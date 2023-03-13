6c6e7f754cd60944e5342f22a593f49d4f1170e3
rveliev <rveliev@ourgapps.com>
2023-03-13 23:24:53 +0300

    up

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,27 +1,65 @@
 package main
 
 import (
+	"compress/gzip"
 	"flag"
 	"fmt"
-	"github.com/NYTimes/gziphandler"
 	"github.com/caarlos0/env/v6"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"yalerting/cmd/app"
 	"yalerting/cmd/handlers"
 	storageClient "yalerting/cmd/storage"
 )
 
+type gzipWriter struct {
+	http.ResponseWriter
+	Writer io.Writer
+}
+
+func (w gzipWriter) Write(b []byte) (int, error) {
+	// w.Writer будет отвечать за gzip-сжатие, поэтому пишем в него
+	return w.Writer.Write(b)
+}
+func gzipHandle(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// проверяем, что клиент поддерживает gzip-сжатие
+
+		fmt.Println("CON ", strings.Contains(r.Header.Get("Accept-Encoding"), "gzip"))
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			// если gzip не поддерживается, передаём управление
+			// дальше без изменений
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		fmt.Println("FZIPPPPPPPPPPPPPPP")
+		// создаём gzip.Writer поверх текущего w
+		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+		if err != nil {
+			io.WriteString(w, err.Error())
+			return
+		}
+		defer gz.Close()
+
+		w.Header().Set("Content-Encoding", "gzip")
+		// передаём обработчику страницы переменную типа gzipWriter для вывода данных
+		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+	})
+}
 func main() {
 	var cfg app.ServerConfig
 	loadConfiguration(&cfg)
+	fmt.Println(cfg)
 
 	storage := storageClient.NewMemStorage()
 
 	if cfg.Restore {
-		app.RestoreMetrics(storage, cfg)
+		app.RestoreMet	rics(storage, cfg)
 	}
 	//flush metrics to file
 	go app.FlushMetrics(storage, cfg)
@@ -32,7 +70,6 @@ func main() {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(gziphandler.GzipHandler)
 
 	r.Route("/", func(r chi.Router) {
 		r.Get("/", handlers.MetricList(storage))
@@ -49,7 +86,7 @@ func main() {
 	})
 
 	// запуск сервера с адресом localhost, порт 8080
-	err := http.ListenAndServe(cfg.Address, r)
+	err := http.ListenAndServe(cfg.Address, gzipHandle(r))
 	if err != nil {
 		fmt.Println(err)
 	}
