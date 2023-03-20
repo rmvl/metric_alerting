9098e437ae4ac6e8e9cbabda2dd8fdf96fe7dff5
rveliev <rveliev@ourgapps.com>
2023-03-20 22:53:08 +0300

    up

--- cmd/handlers/merichandler.go
+++ cmd/handlers/merichandler.go
@@ -136,7 +136,7 @@ func GetMetricInJSON(storage storageRepository.StorageRepository, cfg app.Server
 			return
 		}
 
-		if len(cfg.Key) > 0 || len(metric.Hash) > 0 {
+		if len(cfg.Key) > 0 {
 			err := app.CheckHash(&metric, cfg.Key)
 			if err != nil {
 				http.Error(rw, "hash not valid", http.StatusBadRequest)
