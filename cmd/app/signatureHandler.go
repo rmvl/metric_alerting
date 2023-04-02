f66f38795cc9693e4499e3767c5879d642cd1b5c
rveliev <rveliev@ourgapps.com>
2023-04-02 17:13:01 +0300

    up

--- cmd/app/signatureHandler.go
+++ cmd/app/signatureHandler.go
@@ -12,9 +12,9 @@ func HashMetric(metric *Metrics, secretKey *string) (string, error) {
 	var dataToHash string
 	switch metric.MType {
 	case "counter":
-		dataToHash = fmt.Sprintf("%s:counter:%d", metric.ID, *metric.Delta)
+		dataToHash = fmt.Sprintf("%s:counter:%d", metric.ID, metric.Delta)
 	case "gauge":
-		dataToHash = fmt.Sprintf("%s:gauge:%f", metric.ID, *metric.Value)
+		dataToHash = fmt.Sprintf("%s:gauge:%f", metric.ID, metric.Value)
 	default:
 		fmt.Println("error")
 		return "", errors.New("metric type is not implemented")
